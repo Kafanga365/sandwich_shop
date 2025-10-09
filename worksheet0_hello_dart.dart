59845d13e0de8f4a1cb0a9e3580c0bf289353248
Will Kafanga <williamkafanga@gmail.comm>
2025-10-09 21:20:37 +0100

    Add initial implementation of sum function with test cases

---
+++ worksheet0_hello_dart.dart
@@ -0,0 +1,15 @@
+// write a function that returns the sum of two integers
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  // Test the sum function
+  int result = sum(5, 3);
+  print('The sum of 5 and 3 is: $result');
+
+  // Test with different values
+  int result2 = sum(10, 20);
+  print('The sum of 10 and 20 is: $result2');
+}
